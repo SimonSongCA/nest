bb7fd8f9c8dc3a20d1f680bebc10f265b9353546
Simon Song <simonsongca@gmail.com>
2021-07-25 18:01:35 -0700

    Auto-transform Payloads to DTO instances

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true, // Enabling "whitelist" feature of ValidationPipe
       forbidNonWhitelisted: true, // Throw errors when whitelisted properties are found
+      transform: true, // Auto-transform Payloads to DTO instances
     })
   );
   await app.listen(3000);
